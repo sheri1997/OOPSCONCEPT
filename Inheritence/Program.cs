11a277813ab5a719e3e27bdcd5a332f5e00c7e50
Shreesh Pandey <shvspandey@gmail.com>
2021-10-10 13:20:41 +0530

    Console.writeline changes made on github

--- Inheritence/Program.cs
+++ Inheritence/Program.cs
@@ -10,7 +10,7 @@ namespace Inheritence
             newCar.Color();
             Console.WriteLine(newCar.brand);
             Console.WriteLine(newCar.modal);
-            
+            Console.WriteLine("Changes made in github");
             
         }
     }
